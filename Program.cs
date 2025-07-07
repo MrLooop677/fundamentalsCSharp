3ecd5580b939535cc91a072f7d3cc04989974c99
Eslam <eslam.mahmoud@watanfd.com>
2025-07-07 20:56:04 +0300

    update

--- Program.cs
+++ Program.cs
@@ -11,19 +11,22 @@
             Console.WriteLine("Estimate for carpet cleaning service");
             Console.WriteLine("Number of small carpets:");
              smallCarpets = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(smallCarpets);
             Console.WriteLine("Number of large carpets:");
              largeCarpets = Convert.ToInt32(Console.ReadLine());
              totalCarpets = smallCarpets * 25 + largeCarpets * 35;
              tax = totalCarpets * 0.06; 
-            Console.WriteLine($"Price per small room: $25");
-            Console.WriteLine("Price per large room: $35");
-            Console.WriteLine($"Cost : {totalCarpets}");
+            Console.WriteLine($"Price per small room: {25:C}");
+            Console.WriteLine($"Price per large room: {35:C}");
+            Console.WriteLine($"Cost : {totalCarpets:C}");
 
-            Console.WriteLine($"Tax :{tax}");
+            Console.WriteLine($"Tax :{tax:C}");
             Console.WriteLine("===============================");
-            Console.WriteLine($"Total estimate :{totalCarpets+tax}");
+            Console.WriteLine($"Total estimate :{totalCarpets+tax :C}");
             Console.WriteLine("This estimate is valid for 30 days");
+
+
+
+
         }
     }
 }
--- obj/Debug/net8.0/cleaningServiceTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/cleaningServiceTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cleaningServiceTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd583f2fc9e90905c566bf5928b0771fa39be28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7600223f2a826f43fd3a6730cdf6bfe685d21b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("cleaningServiceTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cleaningServiceTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
